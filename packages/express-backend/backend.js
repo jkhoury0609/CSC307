00bd915eddbde853a1e373ae340da535c5d11ed8
jkhoury <jakhoury@calpoly.edu>
2026-04-10 12:47:09 -0700

    adding users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,7 +41,19 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+
 app.use(express.json());
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id

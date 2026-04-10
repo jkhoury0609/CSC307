861224f0d74d336d0643299f24f77e4f41e9e115
jkhoury <jakhoury@calpoly.edu>
2026-04-10 12:39:32 -0700

    added find user by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,10 +38,21 @@ const findUserByName = (name) => {
     (user) => user["name"] === name
   );
 };
-
-
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 app.use(express.json());
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
